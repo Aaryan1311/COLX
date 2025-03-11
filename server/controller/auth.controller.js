8fcb2b4a58722ef4d5af59ce14aa92abede546f9
Aaryan1311 <ayt131102@gmail.com>
2025-03-12 00:54:45 +0530

    made listing controllers

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -2,80 +2,12 @@ import User from "../model/user.model.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
-import crypto from "crypto";
-import nodemailer from "nodemailer";
-
-
 
 dotenv.config();
-const otpStorage = {};
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
-  },
-});
-
-const generateOTP = () => {
-  return crypto.randomInt(100000, 999999).toString(); // 6-digit OTP
-};
-
-export const sendOtp = async (req, res) => {
+export const signup = async (req, res) => {
   const { name, email, password, branch, rollNo, phoneNo } = req.body;
-
-  if (!name || !email || !password || !branch || !rollNo || !phoneNo) {
-    return res.status(400).json({ message: "Please fill all details" });
-  }
-
-  // Check if user already exists
-  const existingUser = await User.findOne({ rollNo });
-  if (existingUser) return res.status(400).json({ message: "User with this Roll No already registered" });
-
-  const otp = generateOTP();
-  otpStorage[email] = {
-      otp,
-      password, // Store password temporarily
-      otpExpires: Date.now() + 5 * 60 * 1000, // 5-minute expiry
-  };
-
-  // Send OTP via email
-  const mailOptions = {
-      from: process.env.GMAIL_USER,
-      to: email,
-      subject: "Email Verification OTP",
-      text: `Your OTP is: ${otp}. It is valid for 5 minutes.`,
-  };
-
-  try {
-      await transporter.sendMail(mailOptions);
-      res.json({ message: "OTP sent successfully! Verify to complete signup." });
-  } catch (error) {
-      res.status(500).json({ message: "Error sending OTP", error });
-  }
-};
-
-export const verifyOtpAndSignup = async (req, res) => {
-  const { email, name, password, branch, rollNo, phoneNo,otp } = req.body;
-
-  
-
-  if (!email || !otp) {
-      return res.status(400).json({ message: "Fill All the Details" });
-  }
-
-  const storedData = otpStorage[email];
-
-  if (!storedData) {
-      return res.status(400).json({ message: "OTP expired or not requested" });
-  }
-
-  if (storedData.otp !== otp || storedData.otpExpires < Date.now()) {
-      return res.status(400).json({ message: "Invalid or expired OTP" });
-  }
-  const hashedPassword = await bcrypt.hash(storedData.password, 12);
-
+  const hashedPassword = await bcrypt.hash(password, 12);
   const newUser = new User({
     name,
     email,
@@ -110,11 +42,10 @@ export const signin = async (req, res) => {
       { expiresIn: "1d" }
     );
 
-    const { password: pass, ...rest } = existingUser._doc;
+    const {password:pass, ...rest} =existingUser._doc;
     return res.status(200).json({ rest });
   } catch (error) {
     res.status(500).json({ message: "Signin failed" });
   }
 };
-// qtfx sbpg dcce wlpy
 
